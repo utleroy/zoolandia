926d7bc023f7eda993270a06729c49fe81a93dfd
Bryan Hankins <bryanhankins@me.com>
2017-01-14 17:56:01 -0600

    add initial animal object

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Zoolandia.Animals
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var Dog= new Animal();
+            Dog.Color = "Black";
+            Dog.Name = "Fido";
+            Dog.SignatureNoise = "Bark";
+            Dog.Size = "Large";
+
+            Console.WriteLine($"I have a dog named {Dog.Name}. He can {Dog.SignatureNoise} on command.");
+        }
+    }
+}
---
+++ Zoolandia.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Zoolandia.Animals
+{
+    class Animal
+    {
+        public string Name { get; set; }
+        public string Color { get; set;}
+        public string SignatureNoise { get; set; }
+        public string Size { get; set; }
+            
+        }
+    }
